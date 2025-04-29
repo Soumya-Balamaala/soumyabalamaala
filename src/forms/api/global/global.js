4acf27f4c0acdc023d6edb66b90105f3785ac523
Soumya-Balamaala <soumyabalamaala@gmail.com>
2025-04-29 14:13:45 +0530

    Changes in Axios

--- src/forms/api/global/global.js
+++ src/forms/api/global/global.js
@@ -1,10 +1,12 @@
 import axiosInstance from "../../../../utils/axios";
+import { BASE_URL, CurrentAPIUrl } from "../../../../utils/envconfigs";
 import { apiLinks } from "../Urls";
 
 export const getCountries = async () => {
   const response = await axiosInstance.get(apiLinks.global.countries.get, {
     headers: {
       "Content-Type": "application/JSON",
+      "Access-Control-Allow-Origin": "*",
     },
   });
   return response;
--- utils/envconfigs.js
+++ utils/envconfigs.js
@@ -15,16 +15,16 @@ export const LocalBaseURL = "http://localhost:3000";
 
 export const HostedDBLink = process.env.NEXT_HOSTED_DB_URL;
 
-export const BASE_URL = process.env.NEXT_DOMAIN_URL;
+export const BASE_URL = "https://soumyabalamaala.vercel.app";
 
 // DB Current Usage
 
-// export const CurrentDB = HostedDBLink;
-
 export const CurrentDB = HostedDBLink;
+
+// export const CurrentDB = LocalDBLink;
 
 // URL Current Usage
 
-// export const CurrentAPIUrl  = BASE_URL
-
 export const CurrentAPIUrl = BASE_URL;
+
+// export const CurrentAPIUrl = LocalBaseURL;
